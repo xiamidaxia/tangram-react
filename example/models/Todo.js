12daa05e4a1d400d920b47971c2a09a7830e1325
六娃 <wencheng.lwc@alibaba-inc.com>
2016-07-20 22:57:09 +0800

    delete example

--- example/models/Todo.js
+++
@@ -1,5 +0,0 @@
-import { Model, state } from '../../src'
-
-export default class Todo extends Model {
-  @state count = 3
-}
--- example/models/User.js
+++
@@ -1,9 +0,0 @@
-import { Model, state, action } from '../../src'
-
-export default class User extends Model {
-  @state name = 'abc'
-  @state age = 3
-  @action updateName(name) {
-    this.setState({ name })
-  }
-}
